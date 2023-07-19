aef90a0594e09ecf6998dc43f8c911fa0ad4ca08
Orodikev <kevinyengo@gmail.com>
2023-07-19 06:30:30 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
+
 /**
- *  * main - print _putchar
- *   * is written by mbah
- *    * Return: 0
- *     */
+ * main - print _putchar
+ *
+ * Return:Always 0
+ */
 
 int main(void)
 {
