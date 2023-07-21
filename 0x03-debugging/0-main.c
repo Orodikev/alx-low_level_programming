4aad276fab89b1d4816b54c5f7e66494be9018ae
Orodikev <kevinyengo@gmail.com>
2023-07-20 19:22:47 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - Test function for positive or negative
- * Return: 0
+ * Return: Always 0
  */
 int main(void)
 {
@@ -10,4 +10,4 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 	return (0);
-}
+}		
