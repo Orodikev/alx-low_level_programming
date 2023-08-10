f83d4a833ecdb9ff18f7432d02f022b41f055f08
Orodikev <kevinyengo@gmail.com>
2023-08-10 12:39:47 -0700

    Program files created

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @i: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int i)
+{
+	void *ptr;
+
+	ptr = malloc(i);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
