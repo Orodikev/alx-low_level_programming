3b6f17e139a556ab256d3291339fdee4a0c1c552
Orodikev <kevinyengo@gmail.com>
2023-08-31 13:07:27 -0700

    A function that prints the binary equivalence of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * print_binary - Afunction that prints the binary value of a decimal number
- * @n: number to print in binary
+ * print_binary - A Function to print the binary equivalent of a decimal number
+ * @n: The decimal number to print in binary
  */
 void print_binary(unsigned long int n)
 {
-	int i = 0;
-	int j = 0;
-	unsigned long int c;
+	int i, count = 0;
+	unsigned long int deci;
 
 	for (i = 63; i >= 0; i--)
 	{
-		c = n >> i;
+		deci = n >> i;
 
-		if (c & 1)
+		if (deci & 1)
 		{
 			_putchar('1');
-			c++;
+			count++;
 		}
-		else if (j)
+		else if (count)
 			_putchar('0');
 	}
-	if (!j)
+	if (!count)
 		_putchar('0');
 }
