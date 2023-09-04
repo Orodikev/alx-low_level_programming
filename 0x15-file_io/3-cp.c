fc87c76e3c6cebfdd04ceb1ba2874b15acc03b81
Orodikev <kevinyengo@gmail.com>
2023-09-04 13:43:23 -0700

    Task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: points to the name of the file to create.
+ * @text_content: points to a string to write to the file.
+ *
+ * Return: On success -1.
+ *         Otherwise - -1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (k = 0; text_content[k];)
+			k++;
+	}
+
+	i = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	j = write(i, text_content, k);
+
+	if (i == -1 || j == -1)
+		return (-1);
+
+	close(i);
+
+	return (1);
+}
