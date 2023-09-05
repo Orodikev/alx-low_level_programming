9b877e7226ea08086a872ed16351b288f24c384a
Orodikev <kevinyengo@gmail.com>
2023-09-04 23:30:16 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,50 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
-void close_file(int fd);
+char *create_buff(char *file);
+void funclose(int cf);
 
 /**
- * create_buffer - Reserves  1024 bytes for a buffer.
+ * create_buffer - Afunction to reserve 1024 bytes for a buffer.
  * @file: The name of the file buffer is storing chars for.
  *
  * Return: A pointer to the newly-allocated buffer.
  */
-char *create_buffer(char *file)
+char *create_buff(char *file)
 {
-	char *buff;
+	char *buff_pointer;
 
-	buff = malloc(sizeof(char) * 1024);
+	buff_pointer = malloc(sizeof(char) * 1024);
 
-	if (buff == NULL)
+	if (buff_pointer == NULL)
 	{
 		dprintf(STDERR_FILENO,
 			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
-	return (buff);
+	return (buff_pointer);
 }
 
 /**
- * funclose - A function to  Close the file descriptors.
- * @fil: The file descriptor to be closed.
+ * funclose - A function to  Close file descriptors.
+ * @cf: The file descriptor to be closed.
  */
-void funclose(int fil)
+void funclose(int cf)
 {
-	int i;
+	int c;
 
-	i = close(fil);
+	c = close(cf);
 
-	if (i == -1)
+	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fil);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", cf);
 		exit(100);
 	}
 }
 
 /**
- * cp -A function to Copy the contents of a file to another file.
+ * main - Function to Copy the contents of a file to another file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -56,10 +56,13 @@ void funclose(int fil)
  *              If file_to cannot be created or written to - exit code 99.
  *              If file_to or file_from cannot be closed - exit code 100.
  */
-int cp(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int start, en, rea, wri;
-	char *buff;
+	int start;
+	int en;
+	int rea;
+	int wri;
+	char *buff_pointer;
 
 	if (argc != 3)
 	{
@@ -67,9 +70,9 @@ int cp(int argc, char *argv[])
 		exit(97);
 	}
 
-	buff = create_buffer(argv[2]);
+	buff_pointer = create_buff(argv[2]);
 	start = open(argv[1], O_RDONLY);
-	rea = read(start, buff, 1024);
+	rea = read(start, buff_pointer, 1024);
 	en = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
@@ -77,25 +80,25 @@ int cp(int argc, char *argv[])
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]);
-			free(buff);
+			free(buff_pointer);
 			exit(98);
 		}
 
-		wri = write(en, buff, rea);
+		wri = write(en, buff_pointer, rea);
 		if (en == -1 || wri == -1)
 		{
 			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n", argv[2]);
-			free(buff);
+			free(buff_pointer);
 			exit(99);
 		}
 
-		rea = read(start, buff, 1024);
+		rea = read(start, buff_pointer, 1024);
 		en = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (rea > 0);
 
-	free(buff);
+	free(buff_pointer);
 	funclose(start);
 	funclose(en);
 
