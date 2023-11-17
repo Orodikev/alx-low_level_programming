52a084c9faa21c46fc4ab6268b34acc33f0c960b
Kevin Orodi <kevinyengo@gmail.com>
2023-11-16 22:19:34 -0800

    new node at the beginning

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,15 +12,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *n_node;
-	unsigned int i = 0;
+	unsigned int len = 0;
 
-	while (str[i])
-		i++;
+	while (str[len])
+		len++;
 	n_node = malloc(sizeof(list_t));
 	if (!n_node)
 		return (NULL);
 	n_node->str = strdup(str);
-	n_node->i = i;
+	n_node->len = len;
 	n_node->next = (*head);
 	(*head) = n_node;
 	return (*head);
