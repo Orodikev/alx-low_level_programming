d5824dfdbce3403905a71cac86ed6d3c47bf642a
Kevin Orodi <kevinyengo@gmail.com>
2023-11-21 09:49:40 -0800

    New adjustments

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,8 +35,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			new->next = list->next;
 			list->next = new;
-			i++;
 			return (new);
+			i++;
 		}
 		else
 			list = list->next;
