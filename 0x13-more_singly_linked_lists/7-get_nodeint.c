374cff3aa65887c941fbfbb3794940d14ecc2efa
Kevin Orodi <kevinyengo@gmail.com>
2023-11-21 10:28:31 -0800

    Task 7 adjustments

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,13 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	listint_t *list = head;
 
-	for (i = 0; i < index; i++)
+	while (i < index)
 	{
 		list = list->next;
+		i++;
 
 	}
 
